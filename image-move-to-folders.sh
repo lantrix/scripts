6df09a5756062cce4f68ae6ff76001c9a194578c
Lantrix <lantrix@pobox.com>
2018-12-26 22:08:03 +1100

    More file types

--- image-move-to-folders.sh
+++ image-move-to-folders.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Move images to folders with YYYY-MM in them
 for x in *.jpg; do d=$(date -r "$x" +%Y-%m) ; mkdir -p "$d"; mv -- "$x" "$d/"; done
+for x in *.JPG; do d=$(date -r "$x" +%Y-%m) ; mkdir -p "$d"; mv -- "$x" "$d/"; done
+for x in *.mov; do d=$(date -r "$x" +%Y-%m) ; mkdir -p "$d"; mv -- "$x" "$d/"; done
+for x in *.MOV; do d=$(date -r "$x" +%Y-%m) ; mkdir -p "$d"; mv -- "$x" "$d/"; done
+for x in *.mp4; do d=$(date -r "$x" +%Y-%m) ; mkdir -p "$d"; mv -- "$x" "$d/"; done
+for x in *.MP4; do d=$(date -r "$x" +%Y-%m) ; mkdir -p "$d"; mv -- "$x" "$d/"; done
+for x in *.AAE; do d=$(date -r "$x" +%Y-%m) ; mkdir -p "$d"; mv -- "$x" "$d/"; done
