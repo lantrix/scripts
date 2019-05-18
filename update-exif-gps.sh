116cd29f4a6efc74e89c99dc70e07f7c4dce835c
Lantrix <lantrix@pobox.com>
2019-05-18 18:18:45 +1000

    Preserve FS dates
    Also cleanup input order

--- update-exif-gps.sh
+++ update-exif-gps.sh
@@ -5,22 +5,23 @@
 command -v exiftool >/dev/null 2>&1 || { echo >&2 "I require exiftool but it's not installed. Aborting."; exit 1; }
 
 if [ ! -n "$1" ]; then
-    echo "Usage: `basename $0` 0002.jpg 36.989213 -84.231474"
+    echo "Usage: `basename $0` 36.989213 -84.231474 0002.jpg"
 	echo "Lat/Long must be in Decimal Degrees (DD)"
     exit
 fi
 
 # Inputs & Params for script
-latitude=${2?param missing - latitude}
-longtitude=${3?param missing - longtitude}
+latitude=${1?param missing - latitude}
+longtitude=${2?param missing - longtitude}
+filename=${3?param missing - filename}
 
 if [ $# -gt 3 ]; then
     exitWithErrorMessage "$0: too many arguments"
 fi
 
-ls ${1} >/dev/null 2>&1
+ls ${filename} >/dev/null 2>&1
 if ! [ "$?" = "0" ]; then
-	echo >&2 "I can't find $1. Aborting."; exit 1;
+	echo >&2 "I can't find $filename. Aborting."; exit 1;
 fi
 
 re='^-?[0-9]+([.][0-9]+)?$'
@@ -33,5 +34,5 @@ if ! [[ $longtitude =~ $re ]] ; then
    echo >&2 "Longtitude: $longtitude is not a valid Decimal Degrees. Aborting."; exit 1;
 fi
 
-exiftool -overwrite_original -XMP:GPSLatitude=${latitude} -XMP:GPSLongitude=${longtitude} -P $1
-exiftool $1 |grep -i gps
+exiftool -overwrite_original -preserve -XMP:GPSLatitude=${latitude} -XMP:GPSLongitude=${longtitude} -P $filename
+exiftool -a -G0:1 -s -n -GPS* $filename 
